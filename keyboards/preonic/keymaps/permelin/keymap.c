610c498cbaa27f53f7ad480f91bb265ce815b86d
Per Melin <p@greendale.se>
2019-03-07 13:48:02 +0100

    Move LALT one position

--- keyboards/preonic/keymaps/permelin/keymap.c
+++ keyboards/preonic/keymaps/permelin/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   MT_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  SE_MINS, MT_LSFT, \
-  KC_LALT, KC_LCTL, KC_LCTL, KC_LGUI, MO(1),   MO(2),   KC_SPC,  KC_SPC,  MY_ENT,  KC_RCTL, _______, _______  \
+  KC_LALT, KC_LALT, KC_LCTL, KC_LGUI, MO(1),   MO(2),   KC_SPC,  KC_SPC,  MY_ENT,  KC_RCTL, _______, _______  \
 ),
 
 /*
